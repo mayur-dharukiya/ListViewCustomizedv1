212ecd27ec48b1dc56ef600eead067528f996fbf
mayur-dharukiya <mayur@revature.com>
2022-03-16 10:30:28 -0400

    uploading images

---
+++ app/src/main/java/com/example/listviewcustomizedv1/Plant.kt
@@ -0,0 +1,9 @@
+package com.example.listviewcustomizedv1
+
+data class Plant(
+    val id:Int,
+    val name:String,
+    val description:String,
+    val imageRes:Int
+
+)
---
+++ app/src/main/java/com/example/listviewcustomizedv1/Plants.kt
@@ -0,0 +1,5 @@
+package com.example.listviewcustomizedv1
+
+class Plants {
+
+}
